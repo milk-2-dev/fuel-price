884f456334d690c1264b38eae3b56b7494351fdc
milk <milk00000000000000@gmail.com>
2024-11-07 12:12:43 +0100

    [server] add locations to city & replaced _id with id

--- server/mongodb/models/city.js
+++ server/mongodb/models/city.js
@@ -1,9 +1,22 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const City = new mongoose.Schema({
   name: {type: String, required: true},
-  postCode: {type: Number, required: true}
-})
+  postCode: {type: Number, required: true},
+  location: {
+    longitude: {type: Number, required: true},
+    latitude: {type: Number, required: true},
+    plusCode: {type: String}
+  }
+}, {
+  toJSON: {
+    virtuals: true,
+    transform: (doc, ret) => {
+      ret.id = ret._id; // Дублюємо _id у поле id
+      delete ret._id; // Видаляємо поле _id return ret;
+    }
+  }
+});
 
 const CitySchema = mongoose.model('Cities', City);
 
