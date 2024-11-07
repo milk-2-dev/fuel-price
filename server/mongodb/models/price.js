f97e0d6cf5da3a4efaa8ba41dcb128c39805ae0c
milk <milk00000000000000@gmail.com>
2024-11-07 22:22:20 +0100

    [server] clear duplicates & change price data

--- server/mongodb/models/price.js
+++ server/mongodb/models/price.js
@@ -2,10 +2,15 @@ import mongoose from "mongoose";
 
 const Price = new mongoose.Schema({
   stationInternalId: {type: String, required: true},
-  super: {type: String, required: true},
-  e10: {type: String, required: true},
-  diesel: {type: String, required: true},
-  updatedAt: {type: Date, required: true}
+  e10: {type: Number, required: true},
+  super: {type: Number, required: true},
+  diesel: {type: Number, required: true},
+  updatedAt: {type: Date, required: true},
+  trend: {
+    e10: {type: String},
+    super: {type: String},
+    diesel: {type: String},
+  }
 },{
   toJSON: {
     virtuals: true,
