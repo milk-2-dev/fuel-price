efd7e60093a4d1aff8ea9b8490c94040b3b030e0
milk <milk00000000000000@gmail.com>
2024-11-18 22:32:03 +0100

    [server] add endpoint for price

---
+++ server/controllers/priceController.js
@@ -0,0 +1,13 @@
+import fetch from 'node-fetch';
+
+export const getPrices = async (req, res) => {
+  console.log('hello');
+  try {
+    const response = await fetch('https://www.benzinpreis-aktuell.de/api.v2.php?data=nationwide');
+    const prices = await response.json();
+
+    res.status(200).json({success: true, data: prices});
+  } catch (error) {
+    res.status(500).json({success: false, message: error});
+  }
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import cityRoutes from './cityRoutes.js';
 import fuelStationRoutes from './fuelStationRoutes.js';
+import priceRoutes from './priceRoutes.js';
 
 const router = express.Router();
 
 router.use(cityRoutes);
 router.use(fuelStationRoutes);
+router.use(priceRoutes);
 
 export default router;
