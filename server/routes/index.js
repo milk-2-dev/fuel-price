6b829b1440cf8e37d427397d9bf6a13468d95151
milk <milk00000000000000@gmail.com>
2024-11-19 17:51:03 +0100

    [server] add logic for refresh

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import cityRoutes from './cityRoutes.js';
 import fuelStationRoutes from './fuelStationRoutes.js';
+import refreshRoutes from './refreshRoutes.js';
 
 const router = express.Router();
 
 router.use(cityRoutes);
 router.use(fuelStationRoutes);
+router.use(refreshRoutes);
 
 export default router;
