0139d062505cfa69d1897b2b51d6c22d0731b0c3
milk <milk00000000000000@gmail.com>
2024-11-21 19:18:10 +0100

    [client] city select

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,35 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <CitySelect @onChange="handleChangeCity"/>
+      </el-header>
+      <el-main>
+        <!--    <TheWelcome/>-->
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue';
+
+import { getFuelStations } from '@/api/services/main.service.js';
+
+import TheWelcome from '../components/TheWelcome.vue';
+import CitySelect from '@/components/CitySelect.vue';
+
+
+const fuelStationsList = ref([]);
+
+const handleChangeCity = async (city) => {
+  const fuelStationsData = await getFuelStations({cityId: city.id});
+  fuelStationsList.value = fuelStationsData.data;
+};
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+.app-wrapper {
+  height: 100vh;
+}
+</style>
